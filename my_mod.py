2f0f2ddb05d304d77a6d2e03b1da52da0c756416
Will Zong <willzong@bu.edu>
2022-07-11 02:34:14 -0400

    completed Full Prediction Implementation

--- my_mod.py
+++ my_mod.py
@@ -6,6 +6,10 @@ import pandas as pd
 import numpy as np
 import torch
 import copy
+import torch
+from torch import nn
+from torch.utils.data import Dataset,DataLoader
+import gc
 
 
 def getXY(seq:pd.Series, n_lags:int):
@@ -121,19 +125,123 @@ def prod_pred_next_n(last_n_rows, model, n_steps=4):
         print(preds)
         tmp_stack = np.vstack((tmp_stack, preds))
         prediction.append(preds)
+    
 
     return prediction
 
 
 
+def generate_region_pred(region_df:pd.DataFrame, n_lags:int, epochs:int):
+    x,y = getMultiDXY(region_df, n_lags=n_lags)
+    y_train=y
+    n_features = 20
+    x_train=x.reshape((x.shape[0], x.shape[1], n_features))
+    model = CNN_ForecastNet().to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
+    criterion = nn.MSELoss()
+    train = myDataset(x_train,y_train)
+    train_loader = torch.utils.data.DataLoader(train,batch_size=1,shuffle=False)
+    epochs = epochs
+    for epoch in range(epochs):
+        print('epochs {}/{}'.format(epoch+1,epochs))
+        Train(model, optimizer, train_loader, criterion)
+        #Valid()
+        gc.collect()
     
+    res = region_df.tail(2)
+    pred = prod_pred_next_n(res, model)
+
+    return pred
+
+
+
+def generate_full_pred(full_df:pd.DataFrame, n_epochs=200):
+    name_lst = []
+    for i in range(0,full_df.shape[1],20):
+        name = full_df.columns[i][0]
+        name_lst.append(name)
+
+    pred_lst=[]
+    for region_name in name_lst:
+        region_df = full_df[region_name]
+        pred = generate_region_pred(region_df, n_lags=2, epochs=n_epochs)
+        pred = np.round(pred)
+
+        col_lst=[]
+        for i in range(1,21):
+            col=(region_name, i)
+            col_lst.append(col)
+        
+        cols=pd.MultiIndex.from_tuples(col_lst)
+        pred_lst.append(pd.DataFrame(data=pred, columns=cols))
+    
+    final = pd.concat(pred_lst, axis=1)
+    return final
 
 
 
 
+#----------1-D CNN Model Version 1----------
+import torch
+from torch import nn
+from torch.utils.data import Dataset,DataLoader
+import gc
+device=torch.device("cpu")
+
+class CNN_ForecastNet(nn.Module):
+    def __init__(self):
+        super(CNN_ForecastNet,self).__init__()
+        self.conv1d = nn.Conv1d(2,128,kernel_size=1)
+        self.relu = nn.ReLU(inplace=True)
+        self.fc1 = nn.Linear(128,64)
+        self.fc2 = nn.Linear(64,1)
+        
+    def forward(self,x):
+        x = self.conv1d(x)
+        x = self.relu(x)
+        #print(x.shape)
+        x = x.view(-1,128)
+        #print(x.shape)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        
+        return x
+
+class myDataset(Dataset):
+    def __init__(self,feature,target):
+        self.feature = feature
+        self.target = target
+    
+    def __len__(self):
+        return len(self.feature)
+    
+    def __getitem__(self,idx):
+        item = self.feature[idx]
+        label = self.target[idx]
+        
+        return item,label
 
 
-def generate_full_pred():
-
-
-    return
+def Train(model, optimizer, train_loader, criterion):
+    train_losses = []
+    running_loss = .0
+    
+    model.train()
+    
+    for idx, (inputs,labels) in enumerate(train_loader):
+        inputs=inputs.to(torch.float32)
+        labels=labels.to(torch.float32)
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        optimizer.zero_grad(set_to_none = True)
+        preds = model(inputs.float())
+        loss = criterion(preds,labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss
+        
+    train_loss = running_loss/len(train_loader)
+    train_losses.append(train_loss.detach().numpy())
+    
+    print(f'train_loss {train_loss}')
