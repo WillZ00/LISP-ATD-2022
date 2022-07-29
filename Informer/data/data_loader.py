054508d986b51c8e9bdfa821c199073ed904d4e9
will <will@Griffins-MacBook-Pro.local>
2022-07-29 12:55:05 -0400

    started modification for informer model

--- Informer/data/data_loader.py
+++ Informer/data/data_loader.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import pandas as pd
+import atd2022
 
 import torch
 from torch.utils.data import Dataset, DataLoader
@@ -375,6 +376,101 @@ class Dataset_Pred(Dataset):
     
     def __len__(self):
         return len(self.data_x) - self.seq_len + 1
+
+    def inverse_transform(self, data):
+        return self.scaler.inverse_transform(data)
+
+
+class atdDataset(Dataset):
+    def __init__(self, flag='train', size=None, 
+                 features='S',target='OT', scale=True, inverse=False, timeenc=0, freq='h', cols=None):
+        # size [seq_len, label_len, pred_len]
+        # info
+
+        
+
+
+        if size == None:
+            self.seq_len = 24*4*4
+            self.label_len = 24*4
+            self.pred_len = 24*4
+        else:
+            self.seq_len = size[0]
+            self.label_len = size[1]
+            self.pred_len = size[2]
+        # init
+        assert flag in ['train', 'test', 'val']
+        type_map = {'train':0, 'val':1, 'test':2}
+        self.set_type = type_map[flag]
+        
+        self.features = features
+        self.target = target
+        self.scale = scale
+        self.inverse = inverse
+        self.timeenc = timeenc
+        self.freq = freq
+
+        self.__read_data__()
+
+    # Need to modify
+    def __read_data__(self):
+        df_raw = atd2022.io.read_csv()
+        df_raw.insert(0, "timeStamps", df_raw.index)
+
+        #self.scaler = StandardScaler()
+
+    
+
+        # border shape = [#train, #Vali, #Test]
+        border1s = [0]
+        border2s = [180]
+        border1 = border1s[self.set_type]
+        border2 = border2s[self.set_type]
+        
+
+        
+        cols_data=df_raw.columns[1:]
+        df_data = df_raw[cols_data]
+
+        #if self.scale:
+        #    train_data = df_data[border1s[0]:border2s[0]]
+        #    self.scaler.fit(train_data.values)
+        #    data = self.scaler.transform(df_data.values)
+        #else:
+        #    data = df_data.values
+        data = df_data.values
+
+        df_stamp = df_raw[['timeStamps']][border1:border2]
+        df_stamp["timeStamps"]=df_stamp["timeStamps"].dt.to_timestamp('s')
+        df_stamp['date'] = pd.to_datetime(df_stamp.timeStamps)
+        
+        data_stamp = time_features(df_stamp, timeenc=self.timeenc, freq=self.freq)
+
+        self.data_x = data[border1:border2]
+        if self.inverse:
+            self.data_y = df_data.values[border1:border2]
+        else:
+            self.data_y = data[border1:border2]
+        self.data_stamp = data_stamp
+    
+    def __getitem__(self, index):
+        s_begin = index
+        s_end = s_begin + self.seq_len
+        r_begin = s_end - self.label_len 
+        r_end = r_begin + self.label_len + self.pred_len
+
+        seq_x = self.data_x[s_begin:s_end]
+        if self.inverse:
+            seq_y = np.concatenate([self.data_x[r_begin:r_begin+self.label_len], self.data_y[r_begin+self.label_len:r_end]], 0)
+        else:
+            seq_y = self.data_y[r_begin:r_end]
+        seq_x_mark = self.data_stamp[s_begin:s_end]
+        seq_y_mark = self.data_stamp[r_begin:r_end]
+
+        return seq_x, seq_y, seq_x_mark, seq_y_mark
+    
+    def __len__(self):
+        return len(self.data_x) - self.seq_len- self.pred_len + 1
 
     def inverse_transform(self, data):
         return self.scaler.inverse_transform(data)
