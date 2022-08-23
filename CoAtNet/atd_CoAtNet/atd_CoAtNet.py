de8246c54c38e80ed1c6027815760266ef2fd1e0
WillZ00 <willzong@bu.edu>
2022-08-23 00:38:38 -0400

    fully functioning coatnet model completed

--- CoAtNet/atd_CoAtNet/atd_CoAtNet.py
+++ CoAtNet/atd_CoAtNet/atd_CoAtNet.py
@@ -23,8 +23,27 @@ class ATD_CoAtNet(object):
     def _build_model(self, df):
         model = CoAtNet(in_ch=1,image_size=20).float()
         self.model=model
-        self.df = df
+        self.df = df.stack(level=0).sort_index(level=1)
         return self
+
+    def update_df(self, new_row, region_name):
+        data = self.df
+        #last_idx=tmp.index[-1]
+
+        #tmp.loc[last_idx+1] = new_row
+        #print(new_row)
+
+        last_idx = data.loc[(slice(None), region_name),:].index[-1][0]
+        new_row_s= pd.Series(new_row, name=(last_idx+1, region_name), index=data.columns)
+
+        self.df = data.append(new_row_s).sort_index(level=1)
+        return
+
+    # def insert_one_row(data, pred, region_name):
+    #     last_idx = data.index[-1][0]
+    #     new_row = pd.Series(pred, name=(last_idx+1, region_name), index=data.columns)
+    #     return data.append(new_row)
+    
 
     def _acquire_device(self):
         if self.args.use_gpu:
@@ -38,17 +57,25 @@ class ATD_CoAtNet(object):
         self.device = device
         return device
 
-    def _get_data(self, flag:str):
+    def _get_data(self, region_name:str, flag:str):
         if flag=="train":
-            data_set = atdDataset(df = self.df)
-        else:
-            data_set = atd_Pred(df = self.df)
-        data_loader = DataLoader(
+            data_set = atdDataset(region_name, df = self.df)
+            data_loader = DataLoader(
             data_set,
-            batch_size = self.args.batch_size,
-            shuffle=False,
-            drop_last=True
-        )
+                batch_size = self.args.batch_size,
+                shuffle=False,
+                drop_last=True
+            )
+        else:
+            #print("got here")
+            data_set = atd_Pred(region_name, df = self.df)
+            #print(data_set)
+            data_loader = DataLoader(
+                data_set,
+                batch_size = 1,
+                shuffle=False,
+                drop_last=True
+            )
         return data_loader
 
     def _select_optimizer(self):
@@ -65,7 +92,7 @@ class ATD_CoAtNet(object):
     def train(self):
         model =self.model
         device = self.device
-        train_loader = self._get_data(flag="train")
+        #train_loader = self._get_data(flag="train")
         time_now = time.time()
         #train_steps = len(train_loader)
 
@@ -74,51 +101,69 @@ class ATD_CoAtNet(object):
 
         #train_loss=[]
         model.train()
-        running_loss= .0
-        n_step=0
-        for epoch in range(self.args.train_epochs):
-            train_loss=[]
-            for idx, (inputs, labels) in enumerate(train_loader):
-                inputs=inputs.to(torch.float32)
-                labels=labels.to(torch.float32)
-                inputs = inputs.unsqueeze(dim=1)
-                #labels = labels.unsqueeze(dim=1)
-                inputs = inputs.to(device)
-                labels = labels.to(device)
-                #print('check input/label dim', inputs.shape, labels.shape)
-                model_optim.zero_grad(set_to_none = True)
-                preds = model(inputs.float())
-                #preds = preds.reshape(labels.shape[0], labels.shape[1])
-                #print("check shapes_after", preds.shape, labels.shape)
-                loss = criterion(preds,labels)
-                #print(type(train_loss))
-                train_loss.append(loss.item())
-                loss.backward()
-                model_optim.step()
-                #running_loss += loss
-            
-            train_loss = np.average(train_loss)
-            #train_losses.append(train_loss.cpu().detach().numpy())
-            print(f'train_loss {train_loss}')
+        #running_loss= .0
+        #n_step=0
+        #print(self.df)
+        
+        name_lst = self.df.index.droplevel(0).drop_duplicates().tolist()
+
+        for region_name in name_lst:
+            train_loader = self._get_data(region_name=region_name, flag="train")
+            for epoch in range(self.args.train_epochs):
+                train_loss=[]
+                for idx, (inputs, labels) in enumerate(train_loader):
+                    inputs=inputs.to(torch.float32)
+                    labels=labels.to(torch.float32)
+                    inputs = inputs.unsqueeze(dim=1)
+                    #labels = labels.unsqueeze(dim=1)
+                    inputs = inputs.to(device)
+                    labels = labels.to(device)
+                    #print('check input/label dim', inputs.shape, labels.shape)
+                    model_optim.zero_grad(set_to_none = True)
+                    preds = model(inputs.float())
+                    #preds = preds.reshape(labels.shape[0], labels.shape[1])
+                    #print("check shapes_after", preds.shape, labels.shape)
+                    loss = criterion(preds,labels)
+                    #print(type(train_loss))
+                    train_loss.append(loss.item())
+                    loss.backward()
+                    model_optim.step()
+                    #running_loss += loss
+                
+                train_loss = np.average(train_loss)
+                #train_losses.append(train_loss.cpu().detach().numpy())
+                print(f'train_loss {train_loss}')
 
         return self
 
-    def predict(self):
+    def predict(self, region_name:str):
         model =self.model
         device = self.device
-        pred_loader = self._get_data(flag="pred")
+        pred_loader = self._get_data(region_name, flag="pred")
         time_now = time.time()
 
         model.eval()
-
         preds = []
-        for idx, (inputs, labels) in enumerate(pred_loader):
+        
+        #print(len(pred_loader))
+        for idx, (inputs, mean, std) in enumerate(pred_loader):
+            #print("got here")
+            #print(inputs)
+            #print(inputs.shape)
+            inputs = inputs.unsqueeze(dim=1)
             pred = model(torch.tensor(inputs).to(device).float()).cpu().detach().numpy()
-            print(pred.shape)
+            #pred = (pred*std)+mean
+            #print(pred)
+            #print(type(pred))
+            #print("checkstd",type(std))
+            #print("checkmean",type(mean))
+            std = std.cpu().detach().numpy()
+            std[std==0]=1
+            pred = (pred*std)+mean.cpu().detach().numpy()
             preds.append(pred)
         preds=np.array(preds)
-
-        preds = preds.reshape(-1, preds.shape[-2], preds.shape[-1])
+        #print(preds)
+        #preds = preds.reshape(-1, preds.shape[-2], preds.shape[-1])
 
         return np.squeeze(preds)
 
--- CoAtNet/data/data_loader.py
+++ CoAtNet/data/data_loader.py
@@ -7,14 +7,17 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 
 class atdDataset():
-    def __init__(self, df:pd.DataFrame):
-
+    def __init__(self, region_name:str, df:pd.DataFrame):
+        self.region_name=region_name
         self.df=df
         self.__read_data__()
     
     def __read_data__(self):
         df = self.df
-        self.data = df.values
+
+        #df = df.iloc[:-20].stack(level=0).sort_index(level=1)
+        #print("check input df shape", df.shape)
+        self.data = df.loc[(slice(None), self.region_name),:].iloc[:-20].values
         #print("data_dim", self.data.shape)
 
     def __getitem__(self, index):
@@ -23,35 +26,63 @@ class atdDataset():
         #print(index)
         begin = index
         train_x = self.data[begin:begin+20]
+        train_x, mean, std = self._normalize_data(train_x)
         #print(train_x)
         train_y = self.data[begin+20:begin+20+1]
+
+        train_y = (train_y - mean)/std
+        # train_y = np.array([(train_y[i]-mean[i])/std[i] if std[i] !=0 else train_y[i]-mean[i] for i in range(len(train_y))])
+        #print("params",mean, std)
         #print("trainXY_shape", train_x.shape, train_y.shape)
+        #print("check input x shape", train_x.shape)
         return train_x, train_y
+
+    
+    def _normalize_data(self, data):
+        data_mean = np.average(data, axis=1)
+        data_std = np.std(data, axis=1)
+        data_std[data_std==0]=1
+        data_normalized = (data - data_mean)/data_std
+        return data_normalized, data_mean, data_std
+            
     
     def __len__(self):
-        return len(self.data)-20
+        return len(self.data)-20-1
 
 
 class atd_Pred():
 
-    def __init__(self, df:pd.DataFrame):
-
+    def __init__(self, region_name, df:pd.DataFrame):
+        self.region_name = region_name
         self.df=df
         self.__read_data__()
-    
+        #self.region_name = region_name
+  
+
     def __read_data__(self):
         df = self.df
-        border1=len(df)-20
-        border2=len(df)
-        data = df.values
-        data = data[border1:border2]
-        self.data=data
+        self.data = df.loc[(slice(None), self.region_name),:].values
 
     def __getitem__(self, index):
-        begin = index
-        train_x = self.data[index, index+20]
-        train_y = self.data[index+20+1]
-        return train_x, train_y
+        #if index >= self.df.shape[0]-20:
+        
+        #print(index)
+        begin = len(self.data)
+        pred_x = self.data[begin-20:begin]
+        pred_x, mean, std = self._normalize_data(pred_x)
+        #print(train_x)
+        #train_y = self.data[begin+20:begin+20+1]
+        #print("trainXY_shape", train_x.shape, train_y.shape)
+        return pred_x, mean, std
+
+    def _normalize_data(self, data):
+        data_mean = np.average(data, axis=1)
+        data_std = np.std(data, axis=1)
+        data_std[data_std==0]=1
+        data_normalized = (data - data_mean)/data_std
+        return data_normalized, data_mean, data_std
+
     
     def __len__(self):
-        return len(self.data)
+        return 1
+
